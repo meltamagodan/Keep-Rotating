c01d501ca9b809137c23374205dffd613cafa047
NEON <138321333+meltamagodan@users.noreply.github.com>
2024-09-06 13:16:56 +0300

    Update notification_controller.dart

--- keeprotating/lib/notification_controller.dart
+++ keeprotating/lib/notification_controller.dart
@@ -2,20 +2,6 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:root/root.dart';
 
 class NotificationController {
-  /// Use this method to detect when a new notification or a schedule is created
-  @pragma("vm:entry-point")
-  static Future<void> onNotificationCreatedMethod(
-      ReceivedNotification receivedNotification) async {}
-
-  /// Use this method to detect every time that a new notification is displayed
-  @pragma("vm:entry-point")
-  static Future<void> onNotificationDisplayedMethod(
-      ReceivedNotification receivedNotification) async {}
-
-  @pragma("vm:entry-point")
-  static Future<void> onDismissActionReceivedMethod(
-      ReceivedAction receivedAction) async {}
-
   /// Use this method to detect when the user taps on a notification or action button
   @pragma("vm:entry-point")
   static Future<void> onActionReceivedMethod(
@@ -27,7 +13,7 @@ class NotificationController {
       case '1':
         await Root.exec(cmd: 'wm set-user-rotation lock 1');
         break;
-      case '2':
+      case '3':
         await Root.exec(cmd: 'wm set-user-rotation lock 3');
         break;
     }
